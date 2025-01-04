deec5c40d37e01ae08e828358e22fffd74ec289b
Dinesh-ash <ddinesh@citchennai.net>
2025-01-04 22:19:02 +0530

    Adding backend Files

--- backend/models/Question.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const questionSchema = new mongoose.Schema({
-    question: { type: String, required: true },
-    options: { type: [String], required: true },
-    correctOption: { type: Number, required: true }
-});
-
-module.exports = mongoose.model('Question', questionSchema);
